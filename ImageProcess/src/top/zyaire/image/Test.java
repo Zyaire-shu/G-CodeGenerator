b62e13960b1b63d6a019582c9cdb225457218c63
zyaire <shuzewu@zyaire.top>
2022-01-05 11:05:13 +0800

    Main

---
+++ ImageProcess/src/top/zyaire/image/Test.java
@@ -0,0 +1,7 @@
+package top.zyaire.image;
+
+public class Test {
+    public void echo(){
+        System.out.println("hello");
+    }
+}
---
+++ SerialCom/src/top/zyaire/serial/Main.java
@@ -0,0 +1,7 @@
+package top.zyaire.serial;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
