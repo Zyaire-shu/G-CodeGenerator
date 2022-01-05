f25f1d65ea9463dfb7094982ac40a472cb422597
zyaire <shuzewu@zyaire.top>
2022-01-05 14:33:53 +0800

    Add Spring

---
+++ WebView/src/main/java/top/zyaire/webview/WebViewApplication.java
@@ -0,0 +1,13 @@
+package top.zyaire.webview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebViewApplication.class, args);
+    }
+
+}
---
+++ WebView/src/test/java/top/zyaire/webview/WebViewApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zyaire.webview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
