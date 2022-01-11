1d547a89b36408cec6005c3d5c5ef0fda85916fd
zyaire <shuzewu@zyaire.top>
2022-01-11 16:29:09 +0800

    Add g-code generator fxml

--- ImageProcess/src/main/java/top/zyaire/image/Test.java
+++
@@ -1,7 +0,0 @@
-package top.zyaire.image;
-import top.zyaire.serial.Com;
-public class Test {
-    public static void main(String[] args) {
-        new Com().talk("你好");
-    }
-}
---
+++ gcode-image/src/main/java/top/zyaire/image/Test.java
@@ -0,0 +1,7 @@
+package top.zyaire.image;
+
+public class Test {
+    public void Test(String s){
+        System.out.println(s);
+    }
+}
